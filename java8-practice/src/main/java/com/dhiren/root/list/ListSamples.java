fd87391303b7349c3ce74bbbdd809739eb37a454
Mir <mirashik005@gmail.com>
2019-10-30 20:14:42 +0530

    datastructure stack impl using array and linkedlist

--- java8-practice/src/main/java/com/dhiren/root/list/ListSamples.java
+++ java8-practice/src/main/java/com/dhiren/root/list/ListSamples.java
@@ -1,12 +1,18 @@
 package com.dhiren.root.list;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListSamples {
     public static void main(String[] args) {
         List<String> stringList = Collections.emptyList();
-        stringList.add("Dhiren");
+        //stringList.add("Dhiren");
         System.err.println(stringList);
+
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 7, 3, 4, 8, 2, 9);
+        List<Integer> integerList = integers.stream().sorted().collect(Collectors.toList());
+        System.out.println(integerList);
     }
 }
